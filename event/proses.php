c4a1dfc4721e7585ebe2cf8f913566d67e23872b
ihwanariga07 <ihwanariga.ia@gmail.com>
2024-11-23 10:02:27 +0700

    merubah proses

---
+++ event/proses.php
@@ -0,0 +1,22 @@
+<?php
+#1. koneksikan file ini
+include("../koneksi.php");
+
+#2. mengambil value dari form
+$nm = $_POST['nama_event'];
+$lk = $_POST['lokasi'];
+$tgl = $_POST['tanggal'];
+$jml = $_POST['jumlah_peserta'];
+
+#3. menulis query
+$simpan = "INSERT INTO tbl_event (nama_event,lokasi,tanggal,jumlah_peserta) VALUES ('$nm','$lk','$tgl','jml')";
+
+#4. jalankan query
+$proses = mysqli_query($koneksi, $simpan);
+
+#5. mengalihkan halaman
+//header("location:index.php");
+?>
+<script>
+    document.location="index.php";
+</script>
